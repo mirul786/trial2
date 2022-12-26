f0197c17abbfab99c3fa201f3b9b4dec10309b0b
Mirul Ansari <mirulkhan786@gmail.com>
2022-12-26 08:38:47 +0530

    switch

---
+++ temp.js
@@ -0,0 +1,19 @@
+let days = "Sun";
+
+switch(days){
+  case "Mon" : console.log("Monday")
+    break;
+  case "Tue" : console.log("Tuesday")
+    break;
+  case "Wed" : console.log("Wednesday")
+    break;
+  case "Thu" : console.log("Thursday")
+    break;
+  case "Fri" : console.log("Friday")
+    break;
+  case "Sat" : console.log("Saturday")
+    break;
+  case "Sun" : console.log("Sunday")
+    break;
+  default :console.log("Your Input is not valid") 
+}
